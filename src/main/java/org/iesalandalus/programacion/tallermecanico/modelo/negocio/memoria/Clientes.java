4ba7e3fde707c5fae7a18f869ab55a6f4b8b761b
Franparra <fransalta77@gmail.com>
2025-03-27 12:24:31 +0100

    Antes del desastre

--- src/main/java/org/iesalandalus/programacion/tallermecanico/modelo/negocio/memoria/Clientes.java
+++ src/main/java/org/iesalandalus/programacion/tallermecanico/modelo/negocio/memoria/Clientes.java
@@ -1,22 +1,25 @@
-package org.iesalandalus.programacion.tallermecanico.modelo.negocio;
+package org.iesalandalus.programacion.tallermecanico.modelo.negocio.memoria;
 
 import org.iesalandalus.programacion.tallermecanico.modelo.TallerMecanicoExcepcion;
 import org.iesalandalus.programacion.tallermecanico.modelo.dominio.Cliente;
+import org.iesalandalus.programacion.tallermecanico.modelo.negocio.IClientes;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
-public class Clientes {
+public class Clientes implements IClientes {
     private List<Cliente> coleccionClientes;
 
     public Clientes(){
         coleccionClientes = new ArrayList<>();
     }
 
+    @Override
     public List<Cliente> get() {
         return new ArrayList<>(coleccionClientes);
     }
+    @Override
     public void insertar(Cliente cliente) throws TallerMecanicoExcepcion {
         Objects.requireNonNull(cliente, "No se puede insertar un cliente nulo.");
         if (coleccionClientes.contains(cliente)){
@@ -24,6 +27,7 @@ public class Clientes {
         }
         coleccionClientes.add(cliente);
     }
+    @Override
     public boolean modificar(Cliente cliente, String nombre, String telefono) throws TallerMecanicoExcepcion {
         Objects.requireNonNull(cliente,"No se puede modificar un cliente nulo.");
         Cliente clienteExistente = buscar(cliente);
@@ -39,11 +43,13 @@ public class Clientes {
         return true;
 
     }
+    @Override
     public Cliente buscar(Cliente cliente){
         Objects.requireNonNull(cliente,"No se puede buscar un cliente nulo.");
         int indice = coleccionClientes.indexOf(cliente);
         return (indice != -1) ? coleccionClientes.get(indice) : null;
     }
+    @Override
     public void borrar(Cliente cliente) throws TallerMecanicoExcepcion {
         Objects.requireNonNull(cliente, "No se puede borrar un cliente nulo.");
         Cliente clienteExistente = buscar(cliente);
